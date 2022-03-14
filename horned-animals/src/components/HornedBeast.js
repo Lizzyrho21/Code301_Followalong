5eef765111ba373d8ea37381cd2190eff28f6863
Lizzy Wiley <Lizzyrho@gmail.com>
2022-03-14 17:41:44 -0500

    added JSON data to dynamically implement on react page

--- horned-animals/src/components/HornedBeast.js
+++ horned-animals/src/components/HornedBeast.js
@@ -4,8 +4,9 @@ export default class HornedBeast extends Component {
   render() {
     return (
       <>
+      {/* When the data is pull in from props, here is how I want to display it */}
       <h2>{this.props.title}</h2>
-      <img src={this.props.imageUrl} alt='horned animal'/>
+      <img style={{width:'200px', height:'200px' }}src={this.props.imageUrl} alt={this.props.keyword}/>
       <p>{this.props.description}</p>
       </>
     )
--- horned-animals/src/components/Main.js
+++ horned-animals/src/components/Main.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import HornedBeast from './HornedBeast'; // pull in the component to pass props down to
+import  {animalData} from '../data';  // let's pull in some pre-written data for our animal wiki
 export default class Main extends Component {
   render() {
     return (
       <>
-      <HornedBeast title='Unicorn' imageUrl='TBA' description='I have one horn!'/>
-      <HornedBeast title='Rhino' imageUrl='TBA' description='How many horns do I have again?'/>
+      {/* Here, I am using the array map method to pass the data to thier respective properties */}
+      {animalData.map((el) => (<HornedBeast title={el.title} imageUrl={el.image_url} description={el.description} keyword={el.keyword}/>))}
+      
       </>
     )
-  }
+  
+}
 }
