180ba943c37400a5f9ff99a44450697a5488f09f
Lizzy Wiley <Lizzyrho@gmail.com>
2022-03-15 16:45:53 -0500

    added bootstrap styling

--- horned-animals/src/components/HornedBeast.js
+++ horned-animals/src/components/HornedBeast.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { Card, Button } from 'react-bootstrap';
+import '../Wiki.css';
+
 
 export default class HornedBeast extends Component {
   constructor(props) {
@@ -10,14 +13,24 @@ export default class HornedBeast extends Component {
   }
   render() {
     return (
-      <>
+      <div className="wiki-container">
+      
+   
+      <Card style={{  width: "20rem", padding: "auto", margin:'auto', height:'70vh', marginBottom:'1.5rem' }}>
+      <Card.Img  style={{height:'40vh'}} variant="top" onClick={() => {this.setState({likes: this.state.likes + 1 })}} src={this.props.imageUrl} alt={this.props.keyword} />
       {/* When the data is pulled in from props, here is how I want to display it */}
-      <h2>{this.props.title}</h2>
+      <Card.Body>
+      <Card.Title>{this.props.title}</Card.Title>
+      <Card.Text>❤️ | {this.state.likes} {this.props.description}</Card.Text>
+      <Button>Click to Learn more</Button>
+      </Card.Body>
       {/* Here, I use a callback func to set the state and update it realtime */}
-      <img onClick={() => {this.setState({likes: this.state.likes + 1 })}} style={{width:'200px', height:'200px' }}src={this.props.imageUrl} alt={this.props.keyword}/>
+
+
+      </Card>
+      
     
-      <p> ❤️ | {this.state.likes} {this.props.description}</p>
-      </>
+      </div>
     )
   }
 }
--- horned-animals/src/components/Main.js
+++ horned-animals/src/components/Main.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import HornedBeast from './HornedBeast'; // pull in the component to pass props down to
 import  {animalData} from '../data';  // let's pull in some pre-written data for our animal wiki
+import { Container, Col, Row} from 'react-bootstrap';
 export default class Main extends Component {
   render() {
     return (
       <>
       {/* Here, I am using the array map method to pass the data to thier respective properties */}
-      {animalData.map((el) => (<HornedBeast title={el.title} imageUrl={el.image_url} description={el.description} keyword={el.keyword}/>))}
-      
+      <Container fluid>
+
+        <Row xs={1} md={3} lg={4}>
+      {animalData.map((el) => (<Col><HornedBeast title={el.title} imageUrl={el.image_url} description={el.description} keyword={el.keyword}/></Col>))}
+      </Row>
+      </Container>
       </>
     )
   
